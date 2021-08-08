d8272ac1cf9880d91206e8254f903edf421d7216
Kauane <kauane.nep@gmail.com>
2021-08-08 17:02:39 -0300

    Criar splash

---
+++ AndroidStudioProjects/Eletromagnetismo/app/src/main/java/com/example/eletromagnetismo/view/Slids.kt
@@ -0,0 +1,47 @@
+package com.example.eletromagnetismo.view
+
+import android.content.Intent
+import android.os.Bundle
+import com.example.eletromagnetismo.R
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
+
+class Slids : IntroActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_splash)
+
+        isButtonBackVisible = false
+        isButtonNextVisible = false
+
+        addSlide(
+            SimpleSlide.Builder()
+                .title("Wellcome")
+                .image(R.drawable.books_purple)
+                .description("Olá seja bem vindo a calculadora de eletromagnetismo , um jeito fácil de estudar e aprender mais !")
+                .background(R.color.white)
+                .backgroundDark(R.color.white)
+                .scrollable(false)
+                .build()
+        )
+
+        addSlide(
+            SimpleSlide.Builder()
+                .title("Vamos lá")
+                .image(R.drawable.calculator_verde)
+                .description("Para usar é fácil, basta escolher qual tipo de equação você deseja realizar e iserir seus  respectivos valore . Bons estudos ! ")
+                .background(R.color.white)
+                .backgroundDark(R.color.white)
+                .scrollable(false)
+                .build()
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
--- AndroidStudioProjects/Eletromagnetismo/app/src/main/java/com/example/eletromagnetismo/view/Splash.kt
+++ AndroidStudioProjects/Eletromagnetismo/app/src/main/java/com/example/eletromagnetismo/view/Splash.kt
@@ -1,47 +1,24 @@
 package com.example.eletromagnetismo.view
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.eletromagnetismo.R
-import com.heinrichreimersoftware.materialintro.app.IntroActivity
-import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
 
-class Splash : IntroActivity() {
+class Splash : AppCompatActivity() {
+
+    lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_splash)
 
-        isButtonBackVisible = false
-        isButtonNextVisible = false
-
-        addSlide(
-            SimpleSlide.Builder()
-                .title("Wellcome")
-                .image(R.drawable.books_purple)
-                .description("Olá seja bem vindo a calculadora de eletromagnetismo , um jeito fácil de estudar e aprender mais !")
-                .background(R.color.white)
-                .backgroundDark(R.color.white)
-                .scrollable(false)
-                .build()
-        )
-
-        addSlide(
-            SimpleSlide.Builder()
-                .title("Vamos lá")
-                .image(R.drawable.calculator_verde)
-                .description("Para usar é fácil, basta escolher qual tipo de equação você deseja realizar e iserir seus  respectivos valore . Bons estudos ! ")
-                .background(R.color.white)
-                .backgroundDark(R.color.white)
-                .scrollable(false)
-                .build()
-        )
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        var intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
