c5c92effa2c739ebfe757474f48914c854cb68f7
Kauane <kauane.nep@gmail.com>
2021-08-09 21:04:10 -0300

    layout das equações

--- AndroidStudioProjects/Eletromagnetismo/app/src/main/java/com/example/eletromagnetismo/fragments/Home.kt
+++ AndroidStudioProjects/Eletromagnetismo/app/src/main/java/com/example/eletromagnetismo/fragments/Home.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.eletromagnetismo.R
 
 class Home : Fragment() {
@@ -13,11 +16,23 @@ class Home : Fragment() {
         fun newInstance() = Home()
     }
 
+    private lateinit var controllerInstance: NavController
+    private lateinit var contentForce: ConstraintLayout
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        contentForce = view.findViewById(R.id.content_force)
+        contentForce.setOnClickListener {
+
+        }
     }
 }
