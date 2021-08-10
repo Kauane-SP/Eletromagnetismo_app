831fe3c5718cea823a4d5965502c34974deb4fe0
joseRobertoMoura <jose.roberto.moura@everis.com>
2021-08-09 23:08:58 -0300

    implementação initview e eventlistener fragment forca

--- AndroidStudioProjects/Eletromagnetismo/app/src/main/java/com/example/eletromagnetismo/fragments/Forca.kt
+++ AndroidStudioProjects/Eletromagnetismo/app/src/main/java/com/example/eletromagnetismo/fragments/Forca.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
 import com.example.eletromagnetismo.R
 
 class Forca : Fragment() {
@@ -13,11 +16,41 @@ class Forca : Fragment() {
         fun newInstance() = Forca()
     }
 
+    private lateinit var cargaEletrica: AppCompatEditText
+    private lateinit var produtoVetorial: AppCompatEditText
+    private lateinit var potencialProdutoVetorial: AppCompatEditText
+    private lateinit var campoEletrico: AppCompatEditText
+    private lateinit var potencialCampoEletrico: AppCompatEditText
+    private lateinit var result: AppCompatTextView
+    private lateinit var btnCalcular: AppCompatButton
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_forca, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView(view)
+        eventListener()
+    }
+
+    private fun eventListener() {
+        btnCalcular.setOnClickListener {
+
+        }
+    }
+
+    private fun initView(view: View) {
+        cargaEletrica = view.findViewById(R.id.text_carga_eletrica)
+        produtoVetorial = view.findViewById(R.id.text_produto_vetorial)
+        potencialProdutoVetorial = view.findViewById(R.id.text_potencia_vetorial)
+        campoEletrico = view.findViewById(R.id.text_campo_cetorial)
+        potencialCampoEletrico = view.findViewById(R.id.text_potencia_campo_vetorial)
+        result = view.findViewById(R.id.tv_result)
+        btnCalcular = view.findViewById(R.id.bottom_calculate)
     }
 }
